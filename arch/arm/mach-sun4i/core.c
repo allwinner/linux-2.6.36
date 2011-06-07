ba64dc10fd159058d80231c3163d185bf94ce017
yanggq <yanggq@allwinnertech.com>
2011-06-07 09:34:27 +0800

    input subsystem modify

--- arch/arm/mach-sun4i/core.c
+++ arch/arm/mach-sun4i/core.c
@@ -220,6 +220,8 @@ static struct map_desc softwinner_io_desc[] __initdata = {
 	{ SW_VA_TIMERC_IO_BASE,      __phys_to_pfn(SW_PA_TIMERC_IO_BASE),         SZ_1K,     MT_DEVICE },
 	{ SW_VA_UART0_IO_BASE,       __phys_to_pfn(SW_PA_UART0_IO_BASE),          SZ_1K,     MT_DEVICE },
 	{ SW_VA_SID_IO_BASE,         __phys_to_pfn(SW_PA_SID_IO_BASE), SZ_1K, MT_DEVICE },
+	{ SW_VA_TP_IO_BASE,         __phys_to_pfn(SW_PA_TP_IO_BASE),   SZ_1K, MT_DEVICE },
+
 };
 
 void __init softwinner_map_io(void)
