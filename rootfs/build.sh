9a38377fcb442b43bee97f807a985d7dbfe2230b
Tom Cubie <tangliang@allwinnertech.com>
2011-06-16 10:20:21 +0800

    Modify the rootfs gen script if the user want to gen a new rootfs

--- rootfs/build.sh
+++ rootfs/build.sh
@@ -4,6 +4,7 @@
 #arg1 - the new rootfs name
 regenerate_rootfs()
 {
+if [ -e sun4i_rootfs.cpio.gz ]; then
     mkdir tmp;
 
     #Extract the rootfs to the tmp dir
@@ -16,6 +17,9 @@ regenerate_rootfs()
     (cd tmp; find . |cpio -o -Hnewc |gzip > ../$1)
 
     rm -rf tmp
+else
+	(cd skel;find . |cpio -o -Hnewc |gzip > ../$1)
+fi
 }
 
 if [ -z "$1" ]; then
