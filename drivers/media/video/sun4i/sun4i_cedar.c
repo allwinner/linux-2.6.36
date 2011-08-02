830dbb78e04f2edc1b2db06ca8021d818059e4fb
huangxin <huangxin@allwinnertech.com>
2011-08-02 11:07:22 +0800

    cedar: add cedar set ve freq

--- drivers/media/video/sun4i/sun4i_cedar.c
+++ drivers/media/video/sun4i/sun4i_cedar.c
@@ -608,9 +608,20 @@ long cedardev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
             clk_enable(dram_veclk);	
 		break;
 		
-		case IOCTL_SET_VE_FREQ:			
-			break;
-		
+		case IOCTL_SET_VE_FREQ:	
+			{
+				int arg_rate = (int)arg;
+				if(arg_rate >= 320){
+					clk_set_rate(ve_moduleclk, pll4clk_rate/3);//ve_moduleclk rate is 320khz
+				}else if((arg_rate >= 240) && (arg_rate < 320)){
+					clk_set_rate(ve_moduleclk, pll4clk_rate/4);//ve_moduleclk rate is 240khz
+				}else if((arg_rate >= 240) && (arg_rate < 320)){
+					clk_set_rate(ve_moduleclk, pll4clk_rate/6);//ve_moduleclk rate is 160khz
+				}else{
+					printk("IOCTL_SET_VE_FREQ set ve freq error,%s,%d\n", __func__, __LINE__);
+				}
+				break;
+			}
         case IOCTL_GETVALUE_AVS2:
 			/* Return AVS1 counter value */
             return readl(cedar_devp->iomap_addrs.regs_avs + 0x88);
