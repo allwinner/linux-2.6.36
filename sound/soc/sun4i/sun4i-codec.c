149e351bd5711c32fb889a49d32fe0c870055793
huangxin <huangxin@allwinnertech.com>
2011-08-12 16:25:57 +0800

    audio codec:change the resume func from mod_time_func to workschedule func

--- sound/soc/sun4i/sun4i-codec.c
+++ sound/soc/sun4i/sun4i-codec.c
@@ -36,7 +36,6 @@
 #include <mach/system.h>
 #include <linux/list.h>
 static int gpio_pa_shutdown = 0;
-
 struct clk *codec_apbclk,*codec_pll2clk,*codec_moduleclk;
 
 static volatile unsigned int capture_dmasrc = 0;
@@ -153,7 +152,11 @@ struct sw_codec{
 	struct snd_card *card;
 	struct snd_pcm *pcm;		
 };
-struct timer_list codec_resume_timer;
+//struct timer_list codec_resume_timer;
+static void codec_resume_events(struct work_struct *work);
+struct workqueue_struct *resume_work_queue;
+static DECLARE_WORK(codec_resume_work, codec_resume_events);
+
 static unsigned int rates[] = {
 	8000,11025,12000,16000,
 	22050,24000,24000,32000,
@@ -1264,14 +1267,15 @@ void snd_sw_codec_free(struct snd_card *card)
 {
   
 }
-static void codec_resume_events(unsigned long arg)
+
+static void codec_resume_events(struct work_struct *work)
 {
 	printk("%s,%d\n",__func__,__LINE__);
-	codec_wr_control(SW_DAC_DPC ,  0x1, DAC_EN, 0x1);  
-	mdelay(20);
+	codec_wr_control(SW_DAC_DPC ,  0x1, DAC_EN, 0x1);
+	msleep(20);
 	//enable PA
 	codec_wr_control(SW_ADC_ACTL, 0x1, PA_ENABLE, 0x1);
-	mdelay(550);
+	msleep(550);
     //enable dac analog
 	codec_wr_control(SW_DAC_ACTL, 0x1, 	DACAEN_L, 0x1);
 	codec_wr_control(SW_DAC_ACTL, 0x1, 	DACAEN_R, 0x1);
@@ -1283,8 +1287,8 @@ static void codec_resume_events(unsigned long arg)
 	codec_wr_control(SW_DAC_ACTL, 0x1, 	DACPAS, 0x1);
 	//mdelay(30);
 		//pa unmute
-//	codec_wr_control(SW_DAC_ACTL, 0x1, PA_MUTE, 0x1);	
-    mdelay(50);
+//	codec_wr_control(SW_DAC_ACTL, 0x1, PA_MUTE, 0x1);
+    msleep(50);
 	printk("====pa turn on===\n");
 	gpio_write_one_pin_value(gpio_pa_shutdown, 1, "audio_pa_ctrl");	
 	/*for clk test*/
@@ -1294,6 +1298,7 @@ static void codec_resume_events(unsigned long arg)
 	//printk("codec_apb CLK:0xf1c20068 is:%x\n", *(volatile int *)0xf1c20068);
 	//printk("[codec resume reg]\n");
 }
+
 static int __init sw_codec_probe(struct platform_device *pdev)
 {
 	int err;
@@ -1399,11 +1404,17 @@ static int __init sw_codec_probe(struct platform_device *pdev)
 	}
 	 gpio_write_one_pin_value(gpio_pa_shutdown, 0, "audio_pa_ctrl");	
 	 codec_init(); 
-	 gpio_write_one_pin_value(gpio_pa_shutdown, 1, "audio_pa_ctrl");	 
-	 setup_timer(&codec_resume_timer, codec_resume_events, (unsigned long)&codec_resume_timer);		
-	 printk("sun4i Audio codec successfully loaded..\n"); 	 
+	 gpio_write_one_pin_value(gpio_pa_shutdown, 1, "audio_pa_ctrl");
+	 //setup_timer(&codec_resume_timer, codec_resume_events, (unsigned long)&codec_resume_timer);
+	 resume_work_queue = create_singlethread_workqueue("codec_resume");
+	 if (resume_work_queue == NULL) {
+        printk("[su4i-codec] try to create workqueue for codec failed!\n");
+		ret = -ENOMEM;
+		goto err_resume_work_queue;
+	}
+	 printk("sun4i Audio codec successfully loaded..\n");
 	 return 0;
-	 
+     err_resume_work_queue:
 	 out:
 		 dev_err(db->dev, "not found (%d).\n", ret);
 	
@@ -1458,7 +1469,8 @@ static int snd_sw_codec_resume(struct platform_device *pdev)
 	if (-1 == clk_enable(codec_moduleclk)){
 		printk("open codec_moduleclk failed; \n");
 	}
-	mod_timer(&codec_resume_timer, jiffies + 0);
+	//mod_timer(&codec_resume_timer, jiffies + 0);
+	queue_work(resume_work_queue, &codec_resume_work);
 	printk("[audio codec]:resume end\n");
 	return 0;	
 }
@@ -1470,7 +1482,7 @@ static int __devexit sw_codec_remove(struct platform_device *devptr)
 	clk_put(codec_pll2clk);
 	// Õ∑≈codec_apbclk ±÷”æ‰±˙
 	clk_put(codec_apbclk);
-	
+//	kfree(codec_resume_timer);
 	snd_card_free(platform_get_drvdata(devptr));
 	platform_set_drvdata(devptr, NULL);
 	return 0;
