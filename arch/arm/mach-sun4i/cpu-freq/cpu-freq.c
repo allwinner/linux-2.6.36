395b7f19980d3a7fdad6498b20c159047be2e717
kevin <kevin@allwinnertech.com>
2011-08-29 10:33:44 +0800

    update dvfs table

--- arch/arm/mach-sun4i/cpu-freq/cpu-freq.c
+++ arch/arm/mach-sun4i/cpu-freq/cpu-freq.c
@@ -43,12 +43,11 @@ struct cpufreq_dvfs {
     unsigned int    volt;   /* voltage for the frequency    */
 };
 static struct cpufreq_dvfs dvfs_table[] = {
-    {.freq = 1104000000, .volt = 1500}, /* core vdd is 1.50v if cpu frequency is (1008Mhz, 1104Mhz] */
     {.freq = 1008000000, .volt = 1400}, /* core vdd is 1.40v if cpu frequency is (960Mhz, 1008Mhz]  */
-    {.freq = 960000000,  .volt = 1350}, /* core vdd is 1.35v if cpu frequency is (912Mhz, 960Mhz]   */
-    {.freq = 912000000,  .volt = 1300}, /* core vdd is 1.30v if cpu frequency is (864Mhz, 912Mhz]   */
-    {.freq = 864000000,  .volt = 1250}, /* core vdd is 1.25v if cpu frequency is (624Mhz, 864Mhz]   */
-    {.freq = 624000000,  .volt = 1200}, /* core vdd is 1.20v if cpu frequency is (432Mhz, 624Mhz]   */
+    {.freq = 960000000,  .volt = 1400}, /* core vdd is 1.40v if cpu frequency is (912Mhz, 960Mhz]   */
+    {.freq = 912000000,  .volt = 1350}, /* core vdd is 1.35v if cpu frequency is (864Mhz, 912Mhz]   */
+    {.freq = 864000000,  .volt = 1300}, /* core vdd is 1.30v if cpu frequency is (624Mhz, 864Mhz]   */
+    {.freq = 624000000,  .volt = 1250}, /* core vdd is 1.25v if cpu frequency is (432Mhz, 624Mhz]   */
     {.freq = 432000000,  .volt = 1200}, /* core vdd is 1.20v if cpu frequency is (0, 432Mhz]        */
     {.freq = 0,          .volt = 1000}, /* end of cpu dvfs table                                    */
 };
