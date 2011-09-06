175bfd7e8740f3f8bcd7b48c583ad78046a17169
kevin <kevin@allwinnertech.com>
2011-09-06 18:43:18 +0800

    disable fb early-suspend

--- kernel/power/fbearlysuspend.c
+++ kernel/power/fbearlysuspend.c
@@ -63,8 +63,13 @@ static void start_drawing_late_resume(struct early_suspend *h)
 
 static struct early_suspend stop_drawing_early_suspend_desc = {
 	.level = EARLY_SUSPEND_LEVEL_STOP_DRAWING,
+#if 0
 	.suspend = stop_drawing_early_suspend,
 	.resume = start_drawing_late_resume,
+#else
+	.suspend = NULL,
+	.resume = NULL,
+#endif
 };
 
 static ssize_t wait_for_fb_sleep_show(struct kobject *kobj,
