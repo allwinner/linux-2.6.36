f7ce26cf2f772f6343434f761e731c7d8ee89dca
Tom Cubie <tangliang@allwinnertech.com>
2011-10-12 13:18:20 +0800

    gsensor: tiny fix

--- drivers/misc/sun4i-vibrator.c
+++ drivers/misc/sun4i-vibrator.c
@@ -32,7 +32,7 @@ static struct hrtimer vibe_timer;
 static spinlock_t vibe_lock;
 static int vibe_state;
 static int vibe_off;
-script_gpio_set_t vibe_gpio;
+static script_gpio_set_t vibe_gpio;
 static unsigned vibe_gpio_handler;
 
 static void set_sun4i_vibrator(int on)
