25ac71af5ab3a6e2172c00ba45ae91dfd54b0446
xujinf <xujinf@allwinnertech.com>
2012-01-12 18:45:01 +0800

    sun4i: usb: usb1/usb2 support OHCI

--- drivers/usb/host/ohci-hcd.c
+++ drivers/usb/host/ohci-hcd.c
@@ -44,6 +44,9 @@
 #include <asm/unaligned.h>
 #include <asm/byteorder.h>
 
+#ifdef  CONFIG_USB_SW_SUN4I_HCI
+#include <mach/system.h>
+#endif
 
 #define DRIVER_AUTHOR "Roman Weissgaerber, David Brownell"
 #define DRIVER_DESC "USB 1.1 'Open' Host Controller (OHCI) Driver"
@@ -762,7 +765,11 @@ static irqreturn_t ohci_irq (struct usb_hcd *hcd)
 	ints = ohci_readl(ohci, &regs->intrstatus);
 
 #ifdef  CONFIG_USB_SW_SUN4I_HCI
-    if(1){
+{
+    enum sw_ic_ver ic_version = MAGIC_VER_A;
+
+    ic_version = sw_get_ic_ver();
+    if(ic_version == MAGIC_VER_A || ic_version == MAGIC_VER_B){
         __u32 HcRhPortStatus = ohci_readl(ohci, &regs->roothub.portstatus[0]);
 
         if((HcRhPortStatus & RH_PS_CSC) && (ints & OHCI_INTR_RHSC)){
@@ -777,6 +784,7 @@ static irqreturn_t ohci_irq (struct usb_hcd *hcd)
 
         return IRQ_HANDLED;
     }
+}
 #endif
 
 	/* Check for an all 1's result which is a typical consequence
